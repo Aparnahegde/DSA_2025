5c3daef161d1a6e067bdb73b26b9b65873ccc93e
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2025-10-11 00:01:13 +0530

    Time: 5 ms (64.90%) | Memory: 43.7 MB (98.43%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x); // Convert to String
-        int n = s.length(); // Store the String length to int n
-
-        for (int i=0; i<n/2; i++) {
-            // We check whether the elements at the same distance from
-            // beginning and from ending are same, if not we return false
-            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        int temp=x;
+        int pal=0;
+        while(temp>0){
+            int rem=temp%10;
+            pal=pal*10+rem;
+            temp/=10;
+        }
+        if(pal==x){
+            return true;
         }
 
-        // if no flaws are found we return true
-        return true;
+        return false;
     }
 }
