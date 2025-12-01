b6b48190044e1f252c747070c8edbc6e9e39d3ee
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2025-12-01 13:20:36 -0500

    Time: 4 ms (94.76%) | Memory: 67 MB (48.49%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,36 @@
-class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer> m=new HashMap<>();
-        List<Integer> l=new ArrayList<>();
-       for (int x : nums) {
-            m.put(x, m.getOrDefault(x, 0) + 1);
-        }
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> m=new HashMap<>();
+//         List<Integer> l=new ArrayList<>();
+//        for (int x : nums) {
+//             m.put(x, m.getOrDefault(x, 0) + 1);
+//         }
 
        
-        for (int i = 1; i <= n; i++) {
-            if (!m.containsKey(i)) {
-                l.add(i);
-            }
+//         for (int i = 1; i <= n; i++) {
+//             if (!m.containsKey(i)) {
+//                 l.add(i);
+//             }
+//         }
+//         return l;
+//     }
+// }
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        boolean[] seen = new boolean[nums.length + 1];
+
+        for (int num : nums)
+            seen[num] = true;
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!seen[i])
+                res.add(i);
         }
-        return l;
+
+        return res;
     }
 }
