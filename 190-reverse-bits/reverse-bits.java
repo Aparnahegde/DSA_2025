75ec61d07b63f3608a7a6dd21f74c96e4dde968f
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2025-12-19 12:55:48 -0500

    Time: 0 ms (100.00%) | Memory: 42.5 MB (36.55%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -3,10 +3,10 @@ class Solution {
     int rev = 0;
 
     for (int i = 0; i < 32; i++) {
-        rev = (rev *2) | (n & 1);  // take last bit and add to rev
-        n =n/2;                     // move to next bit
-    }
+        rev = (rev *2) | (n & 1);  
+        n =n/2;                     
 
+    }
     return rev;
 
 
