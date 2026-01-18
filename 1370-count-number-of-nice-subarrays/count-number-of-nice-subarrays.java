c89422332ce434280426bea533dfe1dd720b8dbf
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-18 02:22:08 -0500

    Time: 7 ms (95.76%) | Memory: 59.8 MB (18.21%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -4,23 +4,17 @@ class Solution {
     }
 
     private int atMost(int[] nums, int k) {
-        int start = 0;
-        int oddCount = 0;
-        int count = 0;
+        int start = 0, odd = 0, count = 0;
 
         for (int end = 0; end < nums.length; end++) {
-            if (nums[end] % 2 != 0) {
-                oddCount++;
-            }
+            odd += nums[end] & 1;
 
-            while (oddCount > k) {
-                if (nums[start] % 2 != 0) {
-                    oddCount--;
-                }
+            while (odd > k) {
+                odd -= nums[start] & 1;
                 start++;
             }
 
-            count += (end - start + 1);
+            count += end - start + 1;
         }
 
         return count;
