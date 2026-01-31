46b40bacd2b3cce3e1dc24190f5064f736d490e2
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-01-31 00:38:29 -0500

    Time: 1 ms (90.89%) | Memory: 43.6 MB (66.32%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,14 +1,35 @@
+// class Solution {
+//     public String gcdOfStrings(String str1, String str2) {
+//         // Check if concatenated strings are equal or not, if not return ""
+//         if (!(str1 + str2).equals(str2 + str1))
+//             return "";
+//         // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+//         int gcd = gcd(str1.length(), str2.length());
+//         return str1.substring(0, gcd);
+//     }
+
+//     private int gcd(int a, int b) {
+//         return b == 0 ? a : gcd(b, a % b);
+//     }
+// }
+
+
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        // Check if concatenated strings are equal or not, if not return ""
-        if (!(str1 + str2).equals(str2 + str1))
+        if (!(str1 + str2).equals(str2 + str1)) {
             return "";
-        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
-        int gcd = gcd(str1.length(), str2.length());
-        return str1.substring(0, gcd);
+        }
+
+        int lenGCD = gcd(str1.length(), str2.length());
+        return str1.substring(0, lenGCD);        
     }
 
-    private int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+    private int gcd(int len1, int len2) {
+        while (len2 != 0) {
+            int temp = len1 % len2;
+            len1 = len2;
+            len2 = temp;
+        }
+        return len1;
     }
 }
