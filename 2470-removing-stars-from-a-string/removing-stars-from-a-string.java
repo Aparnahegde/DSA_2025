2772bc0d189f5645cac6d8520552767b24aef868
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2026-02-02 12:48:05 -0500

    Time: 63 ms (56.41%) | Memory: 48 MB (68.83%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,15 +1,37 @@
+// class Solution {
+//     public String removeStars(String s) {
+//         StringBuilder sb = new StringBuilder();
+
+//         for (char c : s.toCharArray()) {
+//             if (c == '*') {
+//                 sb.deleteCharAt(sb.length() - 1);
+//             } else {
+//                 sb.append(c);
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String removeStars(String s) {
-        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
             if (c == '*') {
-                sb.deleteCharAt(sb.length() - 1);
+                stack.pop();   // remove previous character
             } else {
-                sb.append(c);
+                stack.push(c);
             }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
         }
 
         return sb.toString();
     }
 }
+
